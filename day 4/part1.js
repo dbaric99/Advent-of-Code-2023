f3a9768a7ee9adddb6734203965c89b2d9679d6a
dbaric99 <damjana.baric1999@gmail.com>
2023-12-04 23:53:09 +0100

    Solved first part of day4

--- day 4/part1.js
+++ day 4/part1.js
@@ -3,7 +3,20 @@ const fileReader = require("../helpers/fileReader.js");
 async function main() {
   const data = await fileReader.getLinesFromText("./puzzle.txt");
 
-  
+  const points = data.map(line => {
+    const [cardInfo, numbers] = line.split(': ');
+    const [winningNumbersStr, cardNumbersStr] = numbers.split(' | ');
+
+    const readNumbers = (x) => x.split(' ').map(Number).filter(Boolean);
+    const winningNumbers = readNumbers(winningNumbersStr);
+    const cardNumbers = readNumbers(cardNumbersStr);
+
+    const matches = winningNumbers.filter(number => cardNumbers.includes(number));
+    return matches.length > 0 ? Math.pow(2, matches.length - 1) : 0;
+  });
+
+  const sum = points.reduce((a, b) => a + b, 0);
+  console.log(sum);
 }
 
 main();
